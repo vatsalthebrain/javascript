57dc66227b23af45b82ecdd47c907c4a43efa5c1
Vatsal Arora <vatsalarora@Vatsals-MacBook-Air.local>
2025-07-04 02:35:31 +0530

    Initial commit

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "vatsalarora2004@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// accountId not allowed to be changed as it is a const 
+
+accountEmail = "hc.com"
+accountPassword = 221
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// dont use var because of issue inblock scope and functional scope
---
+++ 02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as a newer version 
+
+//alert("hello") // we are using node.js not browser
+
+let name = "vatsal"
+let age = 20
+let isLoggedin = false
+
+//bigint use for large numbers
+//null -> standalone value
+// undefined -> value not defined yet
+// symbol 
+
+console.log(typeof "vatsal") // used to get the type of variable wheather it is a string or a int or something else
+
+//null->object
+//undefined->undefined
